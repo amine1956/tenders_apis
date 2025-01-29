095a5127d71ffbb33f56f243d2fb57dbb611e72a
rhahni <oussama.ord@gmail.com>
2025-01-29 19:37:23 +0100

    1

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-cccccc
-oui gfgfgfg
+from typing import Union
+
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+uvicorn.run(app, port=8000, host= '127.0.0.1')
